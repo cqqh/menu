c5f1e2737e7c57b09a361d2cca161ee891200b0c
cqqh <cqqh1@qq.com>
2015-09-30 11:59:51 +0800

    模块设置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,6 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    // 关闭多模块访问
+    'MULTI_MODULE'          =>  false,
+    'DEFAULT_MODULE'        =>  'Home',
     
     // 加载扩展配置文件
     'LOAD_EXT_CONFIG' => 'db',
