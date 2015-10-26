feb267ff42e0c8d25e6703edf72a3eb70ccdd088
cqqh <cqqh1@qq.com>
2015-10-26 17:21:41 +0800

    submit

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,9 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $res = M('Dish')->select();
-
-        foreach($res as $data){
+        foreach(M('Dish')->select() as $data){
             $list[$data['type']][] = $data;
         }
 
@@ -20,7 +18,40 @@ class IndexController extends Controller {
         var_dump($res);
     }
 
+    public function preview(){
+        //var_dump(array_keys($_POST));
+
+        $map['id'] = ['in', array_keys($_POST)];
+        $quantity = 0;
+        $total = 0;
+        foreach(M('dish')->where($map)->select() as $data){
+            if(!$_POST[$data['id']]) continue;
+            $data['quantity'] = $_POST[$data['id']];
+            $data['total'] = $data['quantity'] * $data['price'];
+            $list[$data['id']] = $data;
+            $quantity +=$data['quantity'];
+            $total +=$data['total'];
+        }
+
+        $this->list = $list;
+        $this->quantity = $quantity;
+        $this->total = $total;
+        $this->display();
+    }
+
     public function submit(){
-        var_dump($_POST);
+        $map['id'] = ['in', array_keys($_POST)];
+        $quantity = 0;
+        $total = 0;
+        foreach(M('dish')->where($map)->select() as $data){
+            if(!$_POST[$data['id']]) continue;
+            $data['quantity'] = $_POST[$data['id']];
+            $data['total'] = $data['quantity'] * $data['price'];
+            $list[$data['id']] = $data;
+            $quantity +=$data['quantity'];
+            $total +=$data['total'];
+        }
+
+        echo json_encode($list);
     }
 }
