785fbeb7ddfafc6b7babe70cb2185408ef107244
cqqh <cqqh1@qq.com>
2015-10-30 15:58:02 +0800

    优化

--- Application/Home/Controller/ConsoleController.class.php
+++ Application/Home/Controller/ConsoleController.class.php
@@ -12,6 +12,23 @@ use Think\Controller;
 class ConsoleController extends Controller
 {
     public function index(){
-        echo 'console';
+        foreach(M('dish')->select() as $data){
+            $dish[$data['id']] = $data;
+        }
+
+        $list = M('order_dish')->field('dr_order_dish.*,dr_order.table_num')->where(['status' => 0])->join('dr_order ON dr_order.id = dr_order_dish.order_id')->select();
+
+        $this->list = $list;
+        $this->dish = $dish;
+        $this->display();
+    }
+
+    public function setStatus(){
+        $id = I('post.id');
+        $status = I('post.status');
+        $res = M('order_dish')->where(['id' => $id])->save(['status' => $status]);
+        if($res){
+            echo json_encode(['status' => 0,'info' => 'success']);
+        }
     }
 }
